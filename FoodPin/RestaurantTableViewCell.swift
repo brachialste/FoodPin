a0449dc8dbbe7a66f3902a6b86e49965634596c6
Raul Carvente <brachialste@gmail.com>
2023-02-14 11:55:21 -0600

    Add restaurant list with favorite item feature using UIActionController.

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Raúl Carvente on 16/11/22.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.tintColor = .systemYellow
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
